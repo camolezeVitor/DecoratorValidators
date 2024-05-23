f4f1892d35de7d07222c81dfbdf3e14353e47005
VitorHugoCD <vitordelantonia@gmail.com>
2024-05-23 15:26:07 -0300

    feat[major]: implemented validations on decorators

--- project/validations/basic-validations/validations-decorators.ts
+++ project/validations/basic-validations/validations-decorators.ts
@@ -18,15 +18,99 @@ const storage = (constructor: any, key: string, fn: Function,
     })
 }
 
-const NotNull = (validationSpecs?: ValidationSpecsProtocol) => {
-    return ({constructor}: any, key: string) => {
-        storage(
-            constructor,
-            key,
-            BasicValidationFns.notNull,
-            validationSpecs
-        )
+namespace BasicValidationsDecorators {
+    const NotNull = (validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.notNull,
+                validationSpecs
+            )
+        }
+    }
+    
+    const NotBlank = (validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.notBlank,
+                validationSpecs
+            )
+        }
+    }
+    
+    const NotUndefined = (validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.notUndefined,
+                validationSpecs
+            )
+        }
+    }
+    
+    const Min = (specs: number, validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.min,
+                validationSpecs,
+                specs
+            )
+        }
+    }
+    
+    const Max = (specs: number, validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.max,
+                validationSpecs,
+                specs
+            )
+        }
+    }
+    
+    const MinLenght = (specs: number, validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.minLenght,
+                validationSpecs,
+                specs
+            )
+        }
+    }
+    
+    const MaxLenght = (specs: number, validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.maxLenght,
+                validationSpecs,
+                specs
+            )
+        }
+    }
+    
+    const Pattern = (specs: string | RegExp, validationSpecs?: ValidationSpecsProtocol) => {
+        return ({constructor}: any, key: string) => {
+            storage(
+                constructor,
+                key,
+                BasicValidationFns.pattern,
+                validationSpecs,
+                specs
+            )
+        }
     }
 }
 
-export { NotNull };
+export { BasicValidationsDecorators };
---
+++ project/validations/index.ts
@@ -0,0 +1,1 @@
+export * from "./basic-validations/validations-decorators";
